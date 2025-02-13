349891845a124a904093990b05e4408c58db390d
elkenzi <moelkenzi@gmail.com>
2025-02-13 13:55:37 +0300

    🎉 chore: remove commitlint subject and type warnings

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,8 +4,8 @@ module.exports = {
     'header-max-length': [2, 'always', 72],
     'body-max-line-length': [2, 'always', 100],
     'footer-max-line-length': [2, 'always', 100],
-    'subject-empty': [1, 'never'],
-    'type-empty': [1, 'never'],
+    'subject-empty': [0, 'never'],
+    'type-empty': [0, 'never'],
     'type-enum': [
       2,
       'always',
